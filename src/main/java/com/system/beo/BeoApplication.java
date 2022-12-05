af408fee867a2cd2ba2b66f3a4a0e401b005bf86
duylue <binhminhbenem2801@gmail.com>
2022-12-05 21:13:36 +0700

    first commit

---
+++ src/main/java/com/system/beo/BeoApplication.java
@@ -0,0 +1,13 @@
+package com.system.beo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/system/beo/BeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.system.beo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
